eceb1e0e99d21f259854593a5821d83219051076
Ricardo-LK <rilucaslk@gmail.com>
2025-09-15 23:51:16 -0300

    Não foi possível adicionar no github.io e não deu tempode fazer na vercel

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 let { processExpression } = require('./logic/processa_expressao');
 
 // Rota de processamento
-app.post('ricardo-lk-github-io.vercel.app/', (req, res) => {
+app.post('/process', (req, res) => {
     try {
         let { expression } = req.body;
         if (!expression) {
