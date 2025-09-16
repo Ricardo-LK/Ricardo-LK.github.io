5749f682569f3cf08c49d444b2d41d7ab600e508
Ricardo-LK <rilucaslk@gmail.com>
2025-09-15 23:45:16 -0300

    r

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 let { processExpression } = require('./logic/processa_expressao');
 
 // Rota de processamento
-app.post('/process', (req, res) => {
+app.post('ricardo-lk-github-io.vercel.app/', (req, res) => {
     try {
         let { expression } = req.body;
         if (!expression) {
